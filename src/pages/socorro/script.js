9d9cd9df1709846036758eb3e5f57caa24f5a801
Yuri Pedrozo <125318143+yuriown@users.noreply.github.com>
2025-03-28 10:34:52 -0300

    Delete src/pages/socorro directory

--- src/pages/socorro/script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("showMessageBtn").addEventListener("click", function() {
-    const message = document.getElementById("message");
-    message.classList.remove("hidden");
-    setTimeout(() => {
-        message.classList.add("visible");
-    }, 100); // Delay para ativar o efeito de fade-in
-});
